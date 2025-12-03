da1ba13c7f18bfb911e54049d86986a71f1106a4
bodoszidi <szidibee@gmail.com>
2025-12-03 21:46:43 +0000

    added new component and style sheet

---
+++ src/WeatherApp.js
@@ -0,0 +1,10 @@
+import './WeatherApp.css';
+
+export default function WeatherApp() {
+    return (    
+        <div className="weather-app">
+            <h1>Welcome to the Weather App</h1>
+            <p>Get the latest weather updates for your location.</p>
+        </div>
+    );
+}
