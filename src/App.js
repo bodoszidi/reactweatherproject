4766b89e0d6f602612e87b31473d4ae25385e4e3
bodoszidi <szidibee@gmail.com>
2025-12-06 09:41:29 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   return (
     <div className="App">
       <WeatherApp />
+      <footer>
+        This project was coded by <a href='https://github.com/bodoszidi' target="_blank">
+          Szidonia Bodo
+        </a> and you can find this on <a href='https://weatherappszb.netlify.app/' target='_blank'>Netlify</a> and also open sourced on <a href='https://github.com/bodoszidi/reactweatherproject' target='_blank'>GitHub</a>
+      </footer>
     </div>
   );
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,38 +3,44 @@ import "./WeatherApp.css";
 export default function WeatherApp() {
     return (
         <div className="WeatherApp">
-            <div className="row">
-                <form>
-                    <div className="col-9">
+            <form>
+                <div className="row m-2">
+                    <div className="col-9 py-4">
                         <input
                             type="search"
                             placeholder="Enter a city"
-                            className="form"
+                            className="form w-100 py-1"
+                            autoFocus="on"
                         />
                     </div>
-                    <div className="col-3">
+                    <div className="col-3 py-4">
                         <input
                             type="submit"
                             value="Search"
-                            className="btn btn-primary"
+                            className="btn btn-primary w-100"
                         ></input>
                     </div>
-                </form>
+                </div>
+            </form>
+            <div className="row m-2">
+                <div classname="col-12">
+                    <h1>Manchester</h1>
+                    <ul>
+                        <li>
+                            <strong>Saturday</strong> 08:21
+                        </li>
+                        <li>Mostly Sunny</li>
+                    </ul>
+                </div>
             </div>
-            <h1>Manchester</h1>
-            <ul>
-                <li>
-                    <strong>Saturday</strong> 08:21
-                </li>
-                <li>Mostly Sunny</li>
-            </ul>
-            <div className="row">
+            <div className="row m-2">
                 <div className="col-6">
                     <img
                         src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
                         alt="sunny"
                     />
-                    8°C
+                    <span className="temp">8</span>
+                    <span className="unit">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
