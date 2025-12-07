0cf3e6affde455a2e557ec58883a356a6472b007
bodoszidi <szidibee@gmail.com>
2025-12-07 09:43:36 +0000

    updated footer -
    added missing spacing to clear all the error messages

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ function App() {
   return (
     <div className="App">
       <WeatherApp />
-      <footer>
-        This project was coded by <a href='https://github.com/bodoszidi' target="_blank">
-          Szidonia Bodo
-        </a> and you can find this on <a href='https://weatherappszb.netlify.app/' target='_blank'>Netlify</a> and also open sourced on <a href='https://github.com/bodoszidi/reactweatherproject' target='_blank'>GitHub</a>
-      </footer>
+      <footer className="text-center py-3 lh-sm">
+      This project was coded by{" "}
+      <a href="https://github.com/bodoszidi" target="_blank" rel="noopener noreferrer">
+        Szidonia Bodo
+      </a>{" "}
+      and you can find this on{" "}
+      <a href="https://weatherappszb.netlify.app/" target="_blank" rel="noopener noreferrer">
+        Netlify
+      </a>{" "}
+      and also open sourced on{" "}
+      <a href="https://github.com/bodoszidi/reactweatherproject" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>
+      .
+    </footer>
     </div>
   );
 }
